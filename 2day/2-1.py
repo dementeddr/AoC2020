242a5e7d7afb1c53ee2f1ee75859d31a310cafe6
Rex Baumeister <rex.baumeister@gmail.com>
2020-12-02 10:31:48 -0500

    Made a nothing change. Needed to test some git functionality

--- 2day/2-1.py
+++ 2day/2-1.py
@@ -7,23 +7,26 @@ input_file = "input-d2.txt"
 if len(sys.argv) > 1:
 	input_file = sys.argv[1]
 
+
 def check_pass(rule, char, pwd):
 	count = pwd.count(char)
 
 	return count >= rule[0] and count <= rule[1]
 
 
-valid_pwds = 0
+def main():
+	valid_pwds = 0
 
-with open(input_file, "r") as fp:
-	
-	for line in fp:
+	with open(input_file, "r") as fp:
 		
-		parts = line.split()
-		rules = list(map(int, parts[0].split('-')))
+		for line in fp:
+			
+			parts = line.split()
+			rules = list(map(int, parts[0].split('-')))
 
-		if check_pass(rules, parts[1][0], parts[2]):
-			valid_pwds += 1
+			if check_pass(rules, parts[1][0], parts[2]):
+				valid_pwds += 1
 
-print(f"The number of valid passwords is: {valid_pwds}")
+	print(f"The number of valid passwords is: {valid_pwds}")
 
+main()
